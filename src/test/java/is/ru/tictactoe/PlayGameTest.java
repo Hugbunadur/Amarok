1bfcc81a63bbefd34aa649061cc9b1d09b623f85
dovydas55 <dovydas55@hotmail.com>
2014-10-30 19:16:32 +0000

    created template for writing unit tests

---
+++ src/test/java/is/ru/tictactoe/PlayGameTest.java
@@ -0,0 +1,16 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+import java.util.*;
+
+public class PlayGameTest {
+    //Write your test here
+    /*
+    @Test
+    public void testName(){
+	//TODO: implement test case
+    }
+    */
+}
