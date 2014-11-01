0cff987dd739b0b223a043b8a56ff6f966addea3
Dovydas <dovydas55@hotmail.com>
2014-11-01 21:53:55 +0000

    update playgameweb

--- src/main/java/is/ru/tictactoe/PlayGameWeb.java
+++ src/main/java/is/ru/tictactoe/PlayGameWeb.java
@@ -1,14 +1,49 @@
-package is.ru.sparkit;
+package is.ru.tictactoe;
 
-/*
 import spark.*;
 import static spark.Spark.*;
 import spark.servlet.SparkApplication;
 
 public class PlayGameWeb implements SparkApplication {
     public static void main(String[] args){
-	//TODO: implement
+		staticFileLocation("/public");
+		SparkApplication playgameweb = new PlayGameWeb();
+		String port = System.getenv("PORT");
+		if (port != null) {
+            setPort(Integer.valueOf(port));
+        }
+        playgameweb.init();
+    }
+
+    public void init(){
+    	final PlayGame playgame = new PlayGame();
+    	final Player player = new Player(); 
+    	final HumanPlayer humplayer = new HumanPlayer();
+    	final ComputerPlayer compplayer = new ComputerPlayer();
+
+        post(new Route("Player vs Computer"){
+        	@Override
+        	public Object handle(Request request, Response response){
+        		humplayer.setUsername(request.queryParams("HumanPlayer"));
+        		response.status(200);
+                return response;
+        	}
+
+        });
+
+        post(new Route("Player vs Player"){
+        	@Override
+        	public Object handle(Request request, Response response){
+        		humplayer.setUsername(request.queryParams("HumanPlayer1"));
+        		humplayer.setUsername(request.queryParams("HumanPlayer2"));
+        		response.status(200);
+                return response;
+        	}
+
+        });
+
+ 
     }
 }
 
-*/
+
