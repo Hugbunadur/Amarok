31ee8ad0c3fee681ba5ad047c726bed65b37fee0
Dovydas <dovydas55@hotmail.com>
2014-11-01 23:55:07 +0000

    reh

--- src/main/java/is/ru/tictactoe/PlayGameWeb.java
+++ src/main/java/is/ru/tictactoe/PlayGameWeb.java
@@ -37,14 +37,14 @@ public class PlayGameWeb implements SparkApplication {
 
 		@Override
 		public Object handle(Request request, Response response){
-		    /*String MYName = humplayerA.getUsername();
+		    String MYName = humplayerA.getUsername();
 		    if(MYName.isEmpty()) {
 			MYName = "the length is 0";
-			return MYName;}
-			else*/
-			//MYName =  humplayerA.getUsername();
-		    String MYName = "blaaaaa"; 
-    return MYName;
+			
+		    else
+			MYName =  humplayerA.getUsername();
+		    
+		    return MYName;
 		}
 
 	    });
