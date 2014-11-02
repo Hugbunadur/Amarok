31c6a71d057d37d68563875ca51122b3f03f0ec3
Telma Gudbjorg Eythorsdottir <telma@i-040a14f7.greenqloud.internal>
2014-11-02 14:07:45 +0000

    jjo

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,38 +1,38 @@
 $(document).ready(function(){
-	$("#btn_continue").click(function(){
+    $("#btn_continue").click(function(){
 
 	if($("#PlayerName").val().length == 0) {
-		alert("You must enter a player name!"); 		
-	}
+	    alert("You must enter a player name!"); 
+	    }
 	else{
-		window.location.href = 'menu.html';
-		}
+	    window.location.href = 'menu.html';
+	    }
 	});
 
-	$("#show").click(function(){
-		$(".playertwo").first().show();
+    $("#show").click(function(){
+	$(".playertwo").first().show();
 	});
 
-	$("#btn_playvsplay").click(function(){
+    $("#btn_playvsplay").click(function(){
 
 	if($("#PlayerName2").val().length == 0) {
-		$('.output').html('Please Enter Name').attr('class', 'alert alert-danger');		
-	}
+	    $('.output').html('Please Enter Name').attr('class', 'alert alert-danger');
+	    }
 	else{
-		window.location.href = 'game.html';
-	}
+	    window.location.href = 'game.html';
+	    }
 	});
 
-	$("#btn_playvscomp").click(function(){
+    $("#btn_playvscomp").click(function(){
 
 	
-		window.location.href = 'game.html';
+	window.location.href = 'game.html';
 	
 	});
 
-	$("#btn_compvscomp").click(function(){
+    $("#btn_compvscomp").click(function(){
 
-		window.location.href = 'game.html';
+	window.location.href = 'game.html';
 	
 	});
 
